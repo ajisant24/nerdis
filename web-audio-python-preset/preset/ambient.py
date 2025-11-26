512e37a391572205ca5313b5b53166a270c5640e
ajisant24 <ajisantosoya@gmail.com>
2025-11-27 00:34:29 +0700

    Update ambient.py

--- web-audio-python-preset/preset/ambient.py
+++ web-audio-python-preset/preset/ambient.py
@@ -1,6 +1,8 @@
-# Ambient preset
 set_bpm(60)
-set_reverb(6)
-play("C3", 3)
-play("G3", 3.5)
-play_sample("angin")
+set_reverb(7)
+
+def ambient():
+    pattern(["C3","G3","E3","D3"], 1)
+    play_sample("angin")
+
+forever(ambient)
